e00338211fffe14cb449b81d41d2cf2d78a07d32
Shaurya <shauryadugar17@gmail.com>
2024-02-19 18:26:49 +0530

    changed app UI and structure of lat_long_finder script. Now user can directly select state in UI and then the locations for these states will be calculated and csv created instantly

--- app.py
+++ app.py
@@ -3,45 +3,76 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 import pydeck as pdk
+from lat_long_finder import generate_lat_lon_csv
+import os
 
-lat_lon_df = pd.read_csv("lat_lon.csv")
 df = pd.read_csv("crop_production.csv")
-df = df[df["State_Name"]=="Maharashtra"]
-df = pd.merge(df, lat_lon_df, on="District_Name")
 
 # Streamlit UI
 st.title('Crop Analysis')
 
-# Selectors for crop_year, crop, and season
-crop_year = st.selectbox('Select Crop Year:', df['Crop_Year'].unique())
-crop = st.selectbox('Select Crop:', df['Crop'].unique())
-season = st.selectbox('Select Season:', df['Season'].unique())
+# Selectors for state, crop_year, crop, and season in a Sidebar
+with st.sidebar:
+    st.write("## Filter Data")
+    state = st.selectbox('Select State: ', df["State_Name"].unique()) 
+    crop_year = st.selectbox('Select Crop Year:', df['Crop_Year'].unique())
+    crop = st.selectbox('Select Crop:', df['Crop'].unique())
+    season = st.selectbox('Select Season:', df['Season'].unique())
+
+lat_lon_file = f"State Files/lat_lon_{'_'.join(word for word in state.split())}.csv"
+
+# If the file doesn't exist, then generate the latitude and longitude coordinates 
+# of the districts by calling the generate_lat_lon_csv from the lat_long_finder script
+if not os.path.isfile(lat_lon_file):
+    generate_lat_lon_csv(state)
+
+
+lat_lon_df = pd.read_csv(lat_lon_file)
+df = df[df["State_Name"]==state]
+
+# Merge the dataframes on the District_Name key
+df = pd.merge(df, lat_lon_df, on="District_Name")
 
 # Filter dataframe based on user selections
 filtered_df = df[(df['Crop_Year'] == crop_year) & (df['Crop'] == crop) & (df['Season'] == season)]
 
 # Pydeck map visualization
-view_state = pdk.ViewState(latitude=filtered_df['lat'].mean(), longitude=filtered_df['lon'].mean(), zoom=5, pitch=50)
+view_state = pdk.ViewState(latitude=filtered_df['lat'].mean(), 
+                           longitude=filtered_df['lon'].mean(), 
+                           zoom=5, 
+                           pitch=50)
 
+# Pydeck Layer for showing cultivation area of each district 
 area_layer = pdk.Layer(
     'ScatterplotLayer',
     data=filtered_df,
     get_position=['lon', 'lat'],
-    get_radius='Area * 10',  
-    get_fill_color=[0, 0, 255],
+    get_radius='Area * 0.3',  
+    get_fill_color=[0, 0, 255, 100],
     pickable=True,
     auto_highlight=True
 )
 
+# Pydeck Layer for showing production of each district
 production_layer = pdk.Layer(
     'ColumnLayer',
     data=filtered_df,
-    get_position=['lon', 'lat', 'Area'],  
-    get_elevation='Production * 10',  
-    get_fill_color=[255, 0, 0, 140], 
+    radius=3000,
+    get_position=['lon', 'lat'],  
+    get_elevation='Production * 5',  
+    get_fill_color=[255, 0, 0, 100], 
     pickable=True,
     auto_highlight=True,
 )
 
+# Tooltip for pickable attribute
+tooltip = {
+    "html": "District: <b>{District_Name}</b>, Production: <b>{Production}</b>, Area: <b>{Area}</b>",
+    "style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000"},
+}
+
 # Render pydeck chart
-st.pydeck_chart(pdk.Deck(layers=[area_layer, production_layer], initial_view_state=view_state))
+st.pydeck_chart(pdk.Deck(layers=[area_layer, production_layer], 
+                         initial_view_state=view_state, 
+                         tooltip=tooltip, 
+                         map_style=pdk.map_styles.DARK))
--- lat_long_finder.py
+++ lat_long_finder.py
@@ -3,29 +3,57 @@ import requests
 import json
 from dotenv import load_dotenv
 import os
+import sys
+import numpy as np
 
-load_dotenv()
+INDIAN_STATE_LIST = ['Andaman and Nicobar Islands', 'Andhra Pradesh', 'Arunachal Pradesh',
+ 'Assam', 'Bihar', 'Chandigarh', 'Chhattisgarh', 'Dadra and Nagar Haveli',
+ 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jammu and Kashmir ',
+ 'Jharkhand', 'Karnataka', 'Kerala', 'Madhya Pradesh', 'Maharashtra', 'Manipur',
+ 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Puducherry', 'Punjab',
+ 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana ', 'Tripura', 'Uttar Pradesh',
+ 'Uttarakhand', 'West Bengal']
 
-state="Maharashtra"
+# Generates a csv file containing the district name, latitude and longitude for a given state. 
+# Districts in the crop_production.csv are used only
+def generate_lat_lon_csv(state):
+    assert state in INDIAN_STATE_LIST, f"'{state}' is not a valid state."
+    lat_lon_file = os.path.join(os.getcwd(), f"State Files/lat_lon_{'_'.join(word for word in state.split())}.csv")
+    if os.path.isfile(lat_lon_file):
+        return
+    load_dotenv()
+    api_key = os.environ.get("GEO_API_KEY")
+    df = pd.read_csv("crop_production.csv")
+    df = df[df["State_Name"]==state]
+    print(df["District_Name"].unique())
+    lat_lon_df = pd.DataFrame(columns=["District_Name", "lat", "lon"])
 
-api_key = os.environ.get("GEO_API_KEY")
+    # Make API calls to get all latitutes and longitudes
+    for district in df["District_Name"].unique():
+        url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={district} {state}&aqi=no"
+        response = json.loads(requests.get(url).content)
+        if 'error' in response or 'location' not in response :
+            print(f"\nCoordinates for {district} could not be found! Enter coordinates manually.")
+            lat_lon_df.loc[len(lat_lon_df)] = [district, np.nan, np.nan]
+            continue
+        elif response["location"]["country"] != "India" or state not in response["location"]["region"]:
+            print(f"\nCoordinates for {district} were not found in the region you were looking for! Enter coordinates manually.")
+            lat_lon_df.loc[len(lat_lon_df)] = [district, np.nan, np.nan]
+            continue
+        lat_lon_df.loc[len(lat_lon_df)] = [district, response["location"]["lat"], response["location"]["lon"]]
 
-df = pd.read_csv("crop_production.csv")
-df = df[df["State_Name"]==state]
-print(df["District_Name"].unique())
-lat_lon_df = pd.DataFrame(columns=["District_Name", "lat", "lon"])
+    lat_lon_df.to_csv(lat_lon_file, index=False)
 
-for district in df["District_Name"].unique():
-    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={district} {state}&aqi=no"
-    response = json.loads(requests.get(url).content)
-    if response["location"] is None :
-        print(f"\nCoordinates for {district} could not be found! Enter coordinates manually.")
-        lat_lon_df.loc[len(lat_lon_df)] = [district, None, None]
-        continue
-    elif response["location"]["country"] != "India" and state not in response["location"]["region"]:
-        print(f"\nCoordinates for {district} were not found in the region you were looking for! Enter coordinates manually.")
-        lat_lon_df.loc[len(lat_lon_df)] = [district, None, None]
-        continue
-    lat_lon_df.loc[len(lat_lon_df)] = [district, response["location"]["lat"], response["location"]["lon"]]
 
-lat_lon_df.to_csv("lat_lon.csv", index=False)
+if __name__ == "__main__":
+    # Check if the user has provided at least one argument for the state
+    if len(sys.argv) < 2:
+        print("Usage: python script_name.py <state>")
+        sys.exit(1)  # Exit the script with a non-zero status to indicate an error
+
+    # Get the state argument from the command line but user will have to pass state in the correct case
+    # eg. 'Tamil Nadu' is correct, but 'tamil nadu'/'tamil Nadu'/'Tamil nadu' are not
+    state = ' '.join(sys.argv[1:])
+
+    # Call the function to process the state
+    generate_lat_lon_csv(state)
